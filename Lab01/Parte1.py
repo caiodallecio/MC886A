55ac7370e75f23dc0abcdac5960aee76663fdd9a
Amd-PC\Amd <caiodallecio@gmail.com>
2017-03-20 19:36:52 -0300

    More code done

--- Lab01/Parte1.py
+++ Lab01/Parte1.py
@@ -1,5 +1,5 @@
-x = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
-y = [-53.9, -28.5, -20.7, -3.6, -9.8, 5.0, 4.2, 5.1, 11.4, 27.4, 44.0]
+X = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+Y = [-53.9, -28.5, -20.7, -3.6, -9.8, 5.0, 4.2, 5.1, 11.4, 27.4, 44.0]
 #     a b c d
 fx = [1,1,1,1]
 
@@ -11,7 +11,7 @@ def Foo(fx,x):
         aux *=x
     return ret
 
-def GradFoo(x):
+def GradFoo(fx,x):
     ret = []
     aux = 1
     for a in reversed(range(fx)):
@@ -19,3 +19,13 @@ def GradFoo(x):
         aux *= x
     return ret
         
+def EsseFoo(fx,x,y):
+    ret = 0
+    for a in range(x):
+        ret += (y[a] - Foo(fx,x(a)))**2
+    return ret
+
+def GradEsseFoo(fx,x,y):
+        
+    
+    
